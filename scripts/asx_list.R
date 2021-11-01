465bb2a6db0c91b8f54822da7749c8a44fd53290
Clem <clemlang@yahoo.com>
2021-11-01 22:18:29 +0800

    added a few more lines test code

--- scripts/asx_list.R
+++ scripts/asx_list.R
@@ -6,3 +6,12 @@ get_stock_list <- read.csv("https://asx.api.markitdigital.com/asx-research/1.0/c
   mutate(name_code = paste( Company.name, Code,  sep = ", "))%>%
   rename(Company = Company.name) %>%
   select(Code, Company, name_code)
+
+
+#test code to extract specific companies
+get_stock_list[grepl(pattern = "Sayona",x = get_stock_list$Company,ignore.case = T)==TRUE,]
+
+get_stock_list[grepl(pattern = "avz",x = get_stock_list$Company,ignore.case = T)==TRUE,]
+
+get_stock_list[grepl(pattern = "aurora lab",x = get_stock_list$Company,ignore.case = T)==TRUE,]
+
