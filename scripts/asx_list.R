7b4119975900d3e023fbc13bb4e0401003e5911e
Clem <clemlang@yahoo.com>
2021-11-07 15:05:26 +0800

    calculated a few % change got started on a function to continue...

--- scripts/asx_list.R
+++ scripts/asx_list.R
@@ -9,6 +9,8 @@ get_stock_list <- read.csv("https://asx.api.markitdigital.com/asx-research/1.0/c
   select(Code, Company, name_code)
 
 
+
+
 #test code to extract specific companies
 get_stock_list[grepl(pattern = "Sayona",x = get_stock_list$Company,ignore.case = T)==TRUE,]
 
@@ -27,12 +29,33 @@ today <- Sys.Date()
 HWK_reference_price <-tq_get("HWK.AX",get = 'stock.prices') %>%
                       filter(date == reference_date) %>%
                       select(close)
+
+#getting today's price - cannot be a weekend
+today <- if(format(Sys.Date(), "%u") > 5) {
+  today <- Sys.Date()-2
+}else{
+  today <- Sys.Date()
+}
+
+# #HWK
+
 HWK_today_price <-  tq_get("HWK.AX",get = 'stock.prices') %>%
                     filter(date == today) %>%
                     select(close)
 
+# error check, should always be false.
+is.na(HWK_today_price)
 
 
+#Difference between reference price and latest price.
+change_in_Price <- HWK_today_price - HWK_reference_price
+
+percentage_change <- (HWK_today_price - HWK_reference_price)/HWK_reference_price * 100
+
+#%difference
 
 
+#next will write a function that takes in a stock code an& reference date and spits out the percentage change in price
 
+percentage_change_function <- function(stock = 'AVZ.AX', start_date = '20210201' ){}
+  
